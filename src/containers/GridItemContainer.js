a532d399cb5da401bfd21002ccfebb7d9175c8bf
jpwyse <jpwyse@gmail.com>
2022-04-25 18:30:05 -0400

    Initial commit

---
+++ src/containers/GridItemContainer.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import GridItem from "../components/GridItem";
+
+const GridItemContainer = ({ title, type, data, children, item, ...props }) => {
+  useEffect(() => {
+    console.log(title);
+  }, []);
+
+  return (
+    <GridItem title={title} type={type} data={data} root={item} {...props}>
+      {children}
+    </GridItem>
+  );
+};
+
+GridItemContainer.propTypes = {
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  item: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  children: PropTypes.array
+};
+
+const mapStateToProps = (state, { item }) => state.data[item];
+
+export default connect(mapStateToProps)(GridItemContainer);
